69afb65ca16afa225c4f4de6f926d8fa7cafb397
kunal nimsatkar <kunal.nimsatkar@gmail.com>
2022-11-23 12:51:44 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Select from "react-select";
+import { selectableOptions } from "./components/data";
 
-function App() {
+const App = () => {
+  const colorStyles = {
+    control: (styles) => ({ ...styles, backgroundColor: "white" }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      return { ...styles, color: data.color };
+    },
+    multiValue: (styles, { data }) => {
+      return {
+        ...styles,
+        backgroundColor: data.color,
+        color: "#fff",
+      };
+    },
+    multiValueLabel: (styles, { data }) => {
+      return {
+        ...styles,
+        color: "#fff",
+      };
+    },
+  };
+  const handleChange = (selectedOptions) => {
+    console.log("handleChange", selectedOptions);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <Select
+          className="input-cont"
+          placeholder="Select an individual"
+          options={selectableOptions}
+          isMulti
+          styles={colorStyles}
+          onChange={handleChange}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
